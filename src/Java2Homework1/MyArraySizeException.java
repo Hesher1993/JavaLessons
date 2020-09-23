1f50377ac528cf5749afb7629d6a19dabe602711
Hesher1993 <Daredevillll49@yandex.ru>
2020-09-23 19:51:32 +0300

    ArraySize Homework 2 Java 2

---
+++ src/Java2Homework1/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package Java2Homework1;
+
+public class MyArraySizeException extends IndexOutOfBoundsException {
+    public MyArraySizeException(){
+        super("Массив не формата 4х4");
+    }
+}
