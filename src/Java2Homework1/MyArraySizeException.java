750eefe226db6b837b0a840df19463bf1d3b1ae5
Hesher1993 <Daredevillll49@yandex.ru>
2020-09-23 20:23:29 +0300

    Java2 Homework2

--- src/Java2Homework1/MyArraySizeException.java
+++ src/Java2Homework1/MyArraySizeException.java
@@ -2,6 +2,6 @@ package Java2Homework1;
 
 public class MyArraySizeException extends IndexOutOfBoundsException {
     public MyArraySizeException(){
-        super("Массив не  формата  4х4");
+        super(" Массив не формата  4х4");
     }
 }
