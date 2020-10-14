e71e91ef8df0a31b5c9d3fdb8f87d80401c5bef2
Hesher1993 <Daredevillll49@yandex.ru>
2020-10-14 20:27:02 +0300

    Homework 7

---
+++ src/TestPack/Lesson7.java
@@ -0,0 +1,15 @@
+package TestPack;
+
+import java.io.IOException;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        Server srv = new Server();
+        try {
+            srv.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
